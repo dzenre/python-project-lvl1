61f493704acf52646305dbab2cd938b42d8c6c70
Aleksandr Lukianenko <lukyanenko.alexander@gmail.com>
2020-11-22 02:23:00 +0300

    Step 3.

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
+"""Main function."""
+from brain_games.cli import welcome_user
+
+
 def main():
+    """Enter user name for a greeting."""
     print('Welcome to the Brain Games!')
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
