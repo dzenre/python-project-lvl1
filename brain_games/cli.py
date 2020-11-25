6b87496ce26c3cfba80e2fe3b95f653153261407
Aleksandr Lukianenko <lukyanenko.alexander@gmail.com>
2020-11-25 14:03:21 +0300

    Fix linter messages

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,8 @@
+"""Module to ask player's name."""
 import prompt
 
 
 def welcome_user():
+    """Ask player's name."""
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print('Hello, {}!'.format(name))  # noqa: WPS421 P101
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Main function."""
 from brain_games.cli import welcome_user
 
 
 def main():
     """Enter user name for a greeting."""
-    print('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')  # noqa: WPS421
     welcome_user()
 
 
